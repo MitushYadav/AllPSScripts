608cf22bb8f4bd07dd4e2265c15eba5b90c11a18
Yadav Mitush <mitush.yadav@gmail.com>
2019-02-21 14:47:43 +0100

    W10reports

---
+++ Traverse-Folder.ps1
@@ -0,0 +1,18 @@
+Function Traverse-Folder {
+    <#
+    .Description
+    Traverses N number to folders up
+    #>
+
+    param(
+        [string]$path,
+        [int]$levels
+    )
+
+    do {
+        $path = [io.path]::GetDirectoryName($path)
+        $levels = $levels - 1      
+    } while ($levels -gt 0)
+
+    return $path
+}
