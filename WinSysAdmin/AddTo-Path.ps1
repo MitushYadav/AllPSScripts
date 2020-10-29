ef520f297d1e30f7012113d0d5ab3cad6246828e
Mitush Yadav <Mitush.Yadav@telenetgroup.be>
2020-10-29 13:13:00 +0100

    addto-path

---
+++ WinSysAdmin/AddTo-Path.ps1
@@ -0,0 +1,21 @@
+﻿Function AddTo-Path {
+  [CmdletBinding()]
+  param(
+    [Parameter(Mandatory=$true)]
+    [string]
+    $FolderPath,
+    [Parameter(Mandatory=$true, ParameterSetName="Position1")]
+    [switch]
+    $Prefix,
+    [Parameter(Mandatory=$true, ParameterSetName="Position2")]
+    [switch]
+    $Suffix
+  )
+  
+  $oldpath = (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).path
+  
+  if($Prefix) { $newPath = "$FolderPath;$oldPath" }
+  if($Suffix) { $newPath = "$oldPath;$FolderPath" }
+  
+  Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value $newPath
+}
